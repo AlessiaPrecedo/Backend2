8a5673b32127715a722aab79670c1cdd0fb59df6
Alessia Precedo <alissiaprecedo@hotmail.com>
2026-07-22 19:37:50 -0300

    Delete Backend2-main directory

--- Backend2-main/src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcrypt";
-
-export async function createHash(password) {
-  return await bcrypt.hash(password, await bcrypt.genSalt(10));
-}
-
-export async function isValidPassword(password, hashedPassword) {
-  return await bcrypt.compare(password, hashedPassword);
-}
--- Backend2-main/tests/setup.js
+++
@@ -1,6 +0,0 @@
-process.env.MONGO_URL =
-  process.env.MONGO_URL || "mongodb://localhost:27017/backend2-test";
-process.env.JWT_SECRET =
-  process.env.JWT_SECRET || "test-secret-solo-para-automated-tests";
-process.env.JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "1h";
-process.env.NODE_ENV = "test";
